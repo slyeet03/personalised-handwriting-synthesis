a7df3bd2ba2ad027658954f98ec2814eb93357ce
slyeet03 <shobhit7419@gmail.com>
2026-09-01 14:42:15 +0530

    made the innit file to accept any paths

--- src/dataset.py
+++ src/dataset.py
@@ -1,38 +1,34 @@
 import numpy as np
+import torch
+from torch.utils.data import Dataset as TorchDataset
 
 import vocab
 
 
-class Dataset:
-    def __init__(self):
-        self.train_path = '../dataset/deepwriting_training.npz'
-        self.validation_path = '../dataset/deepwriting_validation.npz'
-        
-        self.train_data = np.load(self.train_path, allow_pickle=True)
-        self.validation_data = np.load(self.validation_path, allow_pickle=True)
+class Dataset(TorchDataset):
+    def __init__(self,path):
+        self.data = np.load(path, allow_pickle=True)
+        print("files", self.data.files)
 
-        print("Train files", self.train_data.files)
-        print("Validation files", self.validation_data.files)
+        self.dict = {}
 
-        self.train_dict = {}
-        self.val_dict = {}
+        for name in self.data.files:
+            self.dict[name] = self.data[name]
 
-        for name in self.train_data.files:
-            self.train_dict[name] = self.train_data[name]
-
-        for name in self.validation_data.files:
-            self.val_dict[name] = self.validation_data[name]
-
-        self.train_sorted_alphabets = vocab.build_vocab(self.train_dict['alphabet'])
-        self.char_to_idx = vocab.build_char_to_idx(self.train_sorted_alphabets)
-        self.val_sorted_alphabets = vocab.build_vocab(self.val_dict['alphabet'])
-        self.char_to_idx = vocab.build_char_to_idx(self.val_sorted_alphabets)
+        self.sorted_alphabets = vocab.build_vocab(self.dict['alphabet'])
+        self.char_to_idx = vocab.build_char_to_idx(self.sorted_alphabets)
 
     def __len__(self):
-        return (self.train_dict['strokes'].shape, self.val_dict['strokes'].shape)
+        return int(self.dict['strokes'].shape[0])
+
+    def __getitem__(self,idx):
+        tensor_stroke = torch.tensor(self.dict['strokes'][idx])
+        tensor_text = torch.tensor(vocab.encode_text(self.dict['texts'][idx],self.char_to_idx))
+
+        return (tensor_stroke,tensor_text)
+         
 
 
 
-if __name__ == '__main__':
-    dataset = Dataset()
+#if __name__ == '__main__':
 
