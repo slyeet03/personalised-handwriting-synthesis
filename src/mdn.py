bcaa6a3c8fa8d982fb1ebd82ce49fa53da644ab1
slyeet03 <shobhit7419@gmail.com>
2026-09-08 12:45:00 +0530

    the loss fn for the mdn head

--- src/mdn.py
+++ src/mdn.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,4 +44,37 @@ class MDNHead:
 
         return pi, mu_x, mu_y, sigma_x, sigma_y, rho, e
         
- 
+
+def mdn_loss(pi, mu_x, mu_y, sigma_x, sigma_y, rho, e, true_dx, true_dy, true_pen, mask):
+    # adding one more dimension to true_dx so it can be subtracted from mu_x which has three dimensions cuz mdn
+    true_dx = true_dx.unsqueeze(-1)   # (batch, seq_len, 1)
+    true_dy = true_dy.unsqueeze(-1)
+
+    # (t-y)
+    dx = true_dx - mu_x               # (batch, seq_len, M)
+    dy = true_dy - mu_y               
+
+    # stats and prob normal distribution shit
+    Z = (dx / sigma_x)**2 + (dy / sigma_y)**2 - (2 * rho * dx * dy) / (sigma_x * sigma_y)
+
+    one_minus_rho2 = 1 - rho**2
+
+    # normalizing constant
+    denom = 2 * math.pi * sigma_x * sigma_y * torch.sqrt(one_minus_rho2)
+    N = torch.exp(-Z / (2 * one_minus_rho2)) / denom
+
+    mixture_density = torch.sum(pi * N, dim=-1)   # (batch, seq_len)
+    loss_position = -torch.log(mixture_density + 1e-8)
+
+    # e had 3 dimensions so reducing it to 2 to do further calculations
+    e = e.squeeze(-1)                             # (batch, seq_len)
+    # binary cross entropy loss for the pen up down
+    loss_pen = -(true_pen * torch.log(e + 1e-8) +
+                 (1 - true_pen) * torch.log(1 - e + 1e-8))
+
+    loss_per_timestep = loss_position + loss_pen
+    # neutralising the padding so the additional 0 doesn't fuck up the loss
+    masked_loss = loss_per_timestep * mask
+    final_loss = masked_loss.sum() / mask.sum()
+
+    return final_loss
